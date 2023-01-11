b98b5563d1db0c0c50fbe26ecc88d889c6b5cc51
ShrustiShru <84789190+ShrustiShru@users.noreply.github.com>
2023-01-11 23:52:59 +0530

    Add files via upload

---
+++ Assignment3/Assignment3/MathOperation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class MathOperation
+    {
+        static void Main(string[] args)
+        {
+            START:
+            Console.WriteLine("Enter the 1st number");
+            int firstnum=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the 2nd number");
+            int secondnum=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the operator");
+            var choice=Console.ReadLine();
+
+            while(true)
+            {
+                switch(choice)
+            {
+                    case "+":
+                        var add = firstnum + secondnum;
+                        Console.WriteLine($"Addition of the two numbers is:  {add}" );
+                        break;
+
+                    case "*":
+                        var multi = firstnum * secondnum;
+                        Console.WriteLine($"Multiplication of the two numbers is:   {multi}");
+                        break;
+
+                    case "-":
+                        var sub = firstnum - secondnum;
+                        Console.WriteLine($"Subtraction of the two numbers is:   {sub}");
+                        break;
+
+                    case "/":
+                        float div = firstnum / secondnum;
+                        Console.WriteLine($"Division of the two numbers is:   {div}");
+                        break;
+
+                    default:
+                        throw new Exception("Invalid Output");
+                        break;
+            }
+                goto START;   
+            }
+        }
+    }
+}
