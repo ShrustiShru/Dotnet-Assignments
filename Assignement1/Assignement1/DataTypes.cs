977af9de194b4e66ea2d41935c7eb0e2493eee4e
ShrustiShru <84789190+ShrustiShru@users.noreply.github.com>
2023-01-11 20:50:05 +0530

    Add files via upload

---
+++ Assignement1/Assignement1/DataTypes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignement1
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The range of float is {float.MinValue} and {float.MaxValue}");
+            Console.WriteLine($"The range of int is {int.MinValue} and {int.MaxValue}");
+            Console.WriteLine($"The range of double is {double.MinValue} and {double.MaxValue}");
+            Console.WriteLine($"The range of decimal is {decimal.MinValue} and {decimal.MaxValue}");
+            Console.WriteLine($"The range of long is {long.MinValue} and {long.MaxValue}");
+            Console.WriteLine($"The range of short or Int16 is {short.MinValue} and {short.MaxValue}");
+            //Console.WriteLine($"The range of Int16 is {Int16.MinValue} and {Int16.MaxValue}");
+            Console.WriteLine($"The range of Int64 is {Int64.MinValue} and {Int64.MaxValue}");
+            Console.WriteLine($"The range of byte is {byte.MinValue} and {byte.MaxValue}");
+        }
+    }
+}
---
+++ Assignment2/Assignment2/OddEven.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class OddEven
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the array");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Enter the numbers");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            //////////////Display array elements//////////
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("Entered elements are: ");
+            foreach (var item in array)
+            {
+                Console.Write(item+" " + "\n");
+            }
+
+
+            Console.WriteLine("-----------------------------------------------");
+            foreach (var item1 in array)
+            {
+                if (item1 % 2 == 0)
+                    Console.WriteLine(item1 + " is Even Number");
+                else
+                    Console.WriteLine(item1 + " is Odd Number");
+            }
+        }
+    }
+}
