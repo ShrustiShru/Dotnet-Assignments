d6b7abdf5fd5ba713a207056ccc12ecec47b7dd6
ShrustiShru <84789190+ShrustiShru@users.noreply.github.com>
2023-01-13 23:10:14 +0530

    Add files via upload

---
+++ Assignment4/Assignment4/Array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of an array");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Enter the elements");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (var item in array)
+            {
+                Console.WriteLine("The elements of the Array are: " + item);
+            }
+            
+        }
+    }
+}
